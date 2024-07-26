97700e06ca9a696e9a08d5d3e1d5d992e63d3ac0
DEA ALVERINA <dealverina@gmail.com>
2024-07-26 15:41:43 +0700

    header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,47 @@
+import Logo from "../assets/img/header/logo.svg";
+import { Link } from "react-router-dom";
+import { Socials } from "./Socials";
+import { MobileNav } from "./MobileNav";
 
 export const Header = () => {
   return (
-    <header className="bg-pink-200 fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
-      Header
+    <header className="fixed w-full px-[30px] lg:px-[100px] z-30 h-[100px] lg:h-[140px] flex items-center">
+      <div className="flex flex-col lg:flex-row lg:items-center w-full justify-between">
+        {/* logo */}
+        <Link to={"/"} className="max-w-[200px]">
+          <img src={Logo} />
+        </Link>
+
+        {/* nav - initially hidden - show on desktop mode */}
+        <nav className="hidden xl:flex gap-x-12 font-semibold">
+          <Link
+            to={"/"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Home
+          </Link>
+          <Link
+            to={"/about"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            About
+          </Link>
+          <Link
+            to={"/portfolio"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Portfolio
+          </Link>
+          <Link
+            to={"/contact"}
+            className="text-[#696c6d] hover:text-primary transition"
+          >
+            Contact
+          </Link>
+        </nav>
+      </div>
+      <Socials />
+      <MobileNav />
     </header>
   );
 };
--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -1,7 +1,41 @@
-import React from 'react'
+import {
+  ImInstagram,
+  ImFacebook,
+  ImTwitter,
+  ImPinterest,
+  ImYoutube,
+} from "react-icons/im";
 
 export const Socials = () => {
   return (
-    <div>Socials</div>
-  )
-}
+    <div className="hidden xl:flex ml-24">
+      <ul className="flex gap-x-4">
+        <li>
+          <a href="http://www.facebook.com" target="_blank">
+            <ImFacebook />
+          </a>
+        </li>
+        <li>
+          <a href="http://www.instagram.com" target="_blank">
+            <ImInstagram />
+          </a>
+        </li>
+        <li>
+          <a href="http://www.twitter.com" target="_blank">
+            <ImTwitter />
+          </a>
+        </li>
+        <li>
+          <a href="http://www.pinterest.com" target="_blank">
+            <ImPinterest />
+          </a>
+        </li>
+        <li>
+          <a href="http://www.youtube.com" target="_blank">
+            <ImYoutube />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+};
