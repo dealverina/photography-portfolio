88aad6d410a68ea355f2354872a0fbeeb8f0cb65
DEA ALVERINA <dealverina@gmail.com>
2024-07-27 14:50:29 +0700

    adj(AnimRoutes): add AnimatePresence

--- src/components/AnimRoutes.tsx
+++ src/components/AnimRoutes.tsx
@@ -1,13 +1,17 @@
-import { Route, Routes } from 'react-router-dom'
-import { About, Contact, Home, Portfolio } from '../pages'
+import { Route, Routes, useLocation } from "react-router-dom";
+import { About, Contact, Home, Portfolio } from "../pages";
+import { AnimatePresence } from "framer-motion";
 
 export const AnimRoutes = () => {
+  const location = useLocation();
   return (
-    <Routes>
-      <Route path='/' element={<Home />}></Route>
-      <Route path='/about' element={<About />}></Route>
-      <Route path='/portfolio' element={<Portfolio />}></Route>
-      <Route path='/contact' element={<Contact />}></Route>
-    </Routes>
-  )
-}
+    <AnimatePresence initial={true} mode="wait">
+      <Routes key={location.pathname} location={location}>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/about" element={<About />}></Route>
+        <Route path="/portfolio" element={<Portfolio />}></Route>
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
+    </AnimatePresence>
+  );
+};
